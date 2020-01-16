d13ce9db1ee3d941797480c0931a6e3368c4cad5
cmrnprry <perry.ca@husky.neu.edu>
2020-01-16 18:52:19 -0500

    Lab1
    Should now draw any triangles

--- Lab1_C_Plus_Plus_SoftwareRasterizerPart1/main.cpp
+++ Lab1_C_Plus_Plus_SoftwareRasterizerPart1/main.cpp
@@ -29,6 +29,7 @@
 #include "Color.h"
 #include "TGA.h"
 #include "Maths.h"
+#include <algorithm>
 
 // Create a canvas to draw on.
 TGA canvas(WINDOW_WIDTH,WINDOW_HEIGHT);
@@ -61,14 +62,47 @@ void drawLine(Vec2 v0, Vec2 v1, TGA& image, ColorRGB c){
     }
 }
 
+void drawPixel(int x, int y, ColorRGB c) {
+    canvas.setPixelColor(y, x, c);
+}
+
+float crossProduct(Vec2 u, Vec2 v) {
+    return (u.x * v.y) -(u.y * v.x);
+}
+
 // Draw a triangle
 void triangle(Vec2 v0, Vec2 v1, Vec2 v2,TGA& image, ColorRGB c){
-    if(glFillMode==LINE){
+    if(LINE){
         drawLine(v0,v1,image,c);
         drawLine(v1,v2,image,c);
         drawLine(v2,v0,image,c);
     }
-    // TODO: Draw a filled triangle
+
+    /* get the bounding box of the triangle */
+    
+    int maxX = std::max(v0.x, std::max(v1.x, v2.x));
+    int minX = std::min(v0.x, std::min(v1.x, v2.x));
+    int maxY = std::max(v0.y, std::max(v1.y, v2.y));
+    int minY = std::min(v0.y, std::min(v1.y, v2.y));
+
+    /* spanning vectors of edge (v1,v2) and (v1,v3) */
+    Vec2 vs1 = Vec2(v1.x - v0.x, v1.y - v0.y);
+    Vec2 vs2 = Vec2(v2.x - v0.x, v2.y - v0.y);
+
+    for (int x = minX; x <= maxX; x++)
+    {
+        for (int y = minY; y <= maxY; y++)
+        {
+            Vec2 q = Vec2(x - v0.x, y - v0.y);
+            float s = (float)crossProduct(q, vs2) / crossProduct(vs1, vs2);
+            float t = (float)crossProduct(vs1, q) / crossProduct(vs1, vs2);
+
+            if ((s >= 0) && (t >= 0) && (s + t <= 1))
+            { /* inside triangle */
+                drawPixel(y, x, c);
+            }
+        }
+    }
 }
 
 
@@ -80,7 +114,6 @@ int main(){
     ColorRGB red;
     red.r = 255; red.g = 0; red.b = 0;
         
-    
     // Points for our Line
     Vec2 line[2] = {Vec2(0,0), Vec2(100,100)};
 
@@ -97,7 +130,7 @@ int main(){
     triangle(tri[0],tri[1],tri[2],canvas,red);
 
     // Output the final image
-    canvas.outputTGAImage("graphics_lab2.ppm");
+    canvas.outputTGAImage("graphics_lab1.ppm");
 
     return 0;
 }
